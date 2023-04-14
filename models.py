9c53a9d2743305623079e6e71a5c40fa4de92b61
justin-pyne <jpyne.justin@gmail.com>
2023-04-14 12:26:34 +0900

    removed extra db, import db into models from app

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from app import db
 
-db = SQLAlchemy()
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
