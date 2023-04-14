af9413c429be04554cb45113e34e898f62e9678e
justin-pyne <jpyne.justin@gmail.com>
2023-04-14 12:37:56 +0900

    resolved circular import

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, flash, render_template, redirect, url_for, flash, reque
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-from models import User
 import os
 import re
 
@@ -13,6 +12,7 @@ db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
 
+from models import User
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
