089a37991ab28a4399286b138570924699d6e277
justin-pyne <jpyne.justin@gmail.com>
2023-04-14 12:16:55 +0900

    added recipe and calendarevent models

--- models.py
+++ models.py
@@ -8,3 +8,29 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    title = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    ingredients = db.Column(db.Text, nullable=True)
+    instructions = db.Column(db.Text, nullable=True)
+    image_url = db.Column(db.String(256), nullable=True)
+    creation_date = db.Column(db.DateTime, default=datetime.utcnow)
+    last_modified = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    # Define the relationship with the User model
+    user = db.relationship('User', backref='recipes')
+
+class CalendarEvent(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'))
+    event_date = db.Column(db.Date, nullable=False)
+    event_time = db.Column(db.Time, nullable=True)
+    notes = db.Column(db.Text, nullable=True)
+
+    # Define the relationships with the User and Recipe models
+    user = db.relationship('User', backref='calendar_events')
+    recipe = db.relationship('Recipe', backref='calendar_events')
